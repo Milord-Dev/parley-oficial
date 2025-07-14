327fdc7b2aab959cc99f02b39656027c80c76998
ricardo milord <milord.dev05@gmail.com>
2025-07-13 23:01:26 -0500

    Modificacion del components y separacion de scripts js

---
+++ frontend/js/components.js
@@ -0,0 +1,41 @@
+(() => {
+const App = {
+    htmlElements: {
+        header: document.getElementById("header"),
+        footer: document.getElementById("footer"),
+    },
+
+    init() {
+        App.methods.loadComponent("header", App.htmlElements.header, App.methods.highlightActiveNav);
+        App.methods.loadComponent("footer", App.htmlElements.footer);
+    },
+
+    methods: {
+        loadComponent(componentName, targetElement, callback) {
+            fetch(`../components/${componentName}.html`)
+                .then(response => response.text())
+                .then(html => {
+                    targetElement.innerHTML = html;
+                    if (callback) callback(); // Ejecutar función extra si se pasa
+                })
+            .catch(err => console.error(`Error cargando ${componentName}:`, err));
+        },
+
+        highlightActiveNav() {
+            const path = window.location.pathname;
+            // Ejemplo: "/pages/apuestas.html"
+            const currentPage = path.split("/").pop().replace(".html", "");
+            // Ejemplo: "apuestas"
+
+            const navLinks = document.querySelectorAll("nav a");
+            navLinks.forEach(link => {
+                if (link.dataset.page === currentPage) {
+                    link.classList.add("font-semibold", "underline");
+                }
+            });
+        },
+    },
+};
+
+App.init();
+})();
