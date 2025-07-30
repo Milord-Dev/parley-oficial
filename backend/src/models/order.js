6187a7a8c189d8afe459b0b08fb79fd6b7107c44
Ceimer <castulocastillo28@gmail.com>
2025-07-30 15:30:02 -0500

    se guardan las ordenes de compra en la bd

---
+++ backend/src/models/order.js
@@ -0,0 +1,25 @@
+// models/order.js
+import mongoose from 'mongoose';
+
+const orderSchema = new mongoose.Schema({
+  user_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  checkout_session_id: {
+    type: String,
+    required: true,
+  },
+  amount: {
+    type: Number,
+    required: true,
+  },
+  created_at: {
+    type: Date,
+    default: Date.now,
+  }
+});
+
+const Order = mongoose.model('Order', orderSchema);
+export default Order;
